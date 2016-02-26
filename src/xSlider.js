ecc82915680e8b7935a780d9521a2a293e403392
shangxinbo@geotmt.com <shangxinbo@geotmt.com>
2016-02-26 17:24:37 +0800

    no message

--- src/xSlider.js
+++ src/xSlider.js
@@ -5,7 +5,9 @@
  * @author Xinbo Shang
  *
  */
+
 "use strict";
+
 (function(factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as anonymous module.
@@ -18,28 +20,29 @@
         factory(jQuery || Zepto);
     }
 }(function($) {
-    
-    if(!$) {
+    //jQuery must be required
+    if (!$) {
         return console.warn('xSlider needs jQuery');
     }
-
+    
     $.fn.extend({
         xSlider: function(options) {
             var defaults = {
-                auto: true,
+                autoplay: true,
+                interval: 3000,
+                speed: 400,
                 nav: true,
                 page: false,
                 scrollNum: 1,
-                direction:'ltr',  //ltr rtl
+                direction: 'ltr',  //ltr rtl
                 controls: true,
-                interval: 2000,
-                speed: 300,
                 hover: false,
                 refresh: true
             };
 
             var options = $.extend(defaults, options);
-
+            
+            //Multi element support
             return this.each(function() {
 
                 var cont = $(this),
@@ -53,31 +56,31 @@
 
                 var slider = {
                     init: function() {
-                        roll = options.scrollNum * 100 +'%';
+                        roll = options.scrollNum * 100 + '%';
                         group = Math.ceil(item.length / options.scrollNum); // focus point nums;
-                        working = false;     // when is the slider switching, you can't start a new switch;
-                        timer = '';          // autoplay timer
+                        working = false; // when is the slider switching, you can't start a new switch;
+                        timer = ''; // autoplay timer
 
                         if (item.length > 1) {
                             setUl.css({
-                                'width': ( item.length + options.scrollNum*2 ) * 100 + '%' ,
-                                'margin-left' : - options.scrollNum * 100 + '%',
-                                'left' : '0'
+                                'width': (item.length + options.scrollNum * 2) * 100 + '%',
+                                'margin-left': -options.scrollNum * 100 + '%',
+                                'left': '0'
                             });
                             item.css({
                                 'float': 'left',
                                 'listStyle': 'none',
-                                'width': 100/( item.length + options.scrollNum*2 ) + '%'
+                                'width': 100 / (item.length + options.scrollNum * 2) + '%'
                             });
                             setUl.html(item.slice(0).clone());
-                            setUl.append(item.slice(0,options.scrollNum).clone());
+                            setUl.append(item.slice(0, options.scrollNum).clone());
                             setUl.prepend(item.slice(-options.scrollNum).clone());
                             this.set();
                         }
                     },
                     set: function() {
                         var _this = this;
-                        
+
                         // left / right  buttons
                         if (options.controls) {
                             cont.append('<div class="xslider-arrow"><span class="prev"></span><span class="next"></span></div>');
@@ -93,10 +96,10 @@
                         }
 
 
-                        if (options.nav||options.page) {
+                        if (options.nav || options.page) {
                             var dom = '<div class="xslider-nav">';
 
-                            if(options.nav){
+                            if (options.nav) {
 
                                 for (var i = 0; i < group; i++) {
                                     if (i == 0) {
@@ -134,11 +137,11 @@
                         var _this = this;
                         if (options.auto) {
                             timer = setInterval(function() {
-                                if(options.direction=='left'){
-                                     _this.toPrev();
-                                 }else{
+                                if (options.direction == 'left') {
+                                    _this.toPrev();
+                                } else {
                                     _this.toNext();
-                                 }
+                                }
                             }, options.interval);
                         }
                     },
@@ -150,17 +153,16 @@
                     animate: function(num) {
                         working = true;
                         var _this = this;
-                        console.log(num);
                         setUl.velocity({
-                            left: -num*100 +'%'
-                        }, options.speed, function() {  
-                            if(num<0){
-                                current=group-1;
-                                setUl.css('left',-(group-1)*100+'%');
-                            }else if(num>=group){
+                            left: -num * 100 + '%'
+                        }, options.speed, function() {
+                            if (num < 0) {
+                                current = group - 1;
+                                setUl.css('left', -(group - 1) * 100 + '%');
+                            } else if (num >= group) {
                                 current = 0;
-                                setUl.css('left',0);
-                            }else{
+                                setUl.css('left', 0);
+                            } else {
                                 current = num;
                             }
                             _this.setPosition(current);
@@ -170,23 +172,23 @@
                     toNext: function() {
                         if (!working) {
                             working = true;
-                            var next = current+1;
+                            var next = current + 1;
                             this.animate(next);
                         }
                     },
                     toPrev: function() {
                         if (!working) {
                             working = true;
-                            var next = current-1;
+                            var next = current - 1;
                             this.animate(next);
                         }
                     },
                     setPosition: function(num, dir) {
-                        if(options.nav){
+                        if (options.nav) {
                             cont.find('.xslider-nav').find('span').removeClass('active').eq(num).addClass('active');
                         }
-                        if(options.page){
-                            cont.find('.xslider-nav').find('font').html(num+1);
+                        if (options.page) {
+                            cont.find('.xslider-nav').find('font').html(num + 1);
                         }
                     },
                     destroy: function() {
